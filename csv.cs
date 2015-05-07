4c001780758d20b11f55983f921976022d41223b
Philip Wrinkle <philip.wrinkle@in10sity.net>
2015-05-07 11:44:43 -0400

    Adding classes used for parsing and comparison

---
+++ csv.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace csvDifferences
+{
+    class csv
+    {
+        public List<List<string>> rows;
+        public List<string> headers;
+        public int totalLines;
+
+        public csv(string path)
+        {
+            this.headers = new List<string>();
+            this.rows = new List<List<string>>();
+            int lineNum = 0;
+            using (StreamReader read = new StreamReader(path))
+            {
+                while (!read.EndOfStream)
+                {
+                    string[] row = read.ReadLine().Split(',');
+                    if (lineNum++ == 0)
+                    {
+                        foreach (string cell in row)
+                        {
+                            this.headers.Add(cell);
+                        }
+                    }
+                    else
+                    {
+                        List<string> tosave = new List<string>();
+                        foreach (string cell in row)
+                        {
+                            tosave.Add(cell);
+                        }
+                        this.rows.Add(tosave);
+                    }
+                }
+            }
+            this.totalLines = lineNum;
+        }
+
+        public string getRowForWrite(int index)
+        {
+            List<string> row = this.rows.ElementAt<List<string>>(index);
+            string result = "";
+            string delimiter = "";
+            row.ForEach(delegate(string cell)
+            {
+                result += delimiter + cell;
+                if (delimiter == "")
+                {
+                    delimiter = ",";
+                }
+            });
+            return result;
+        }
+
+        public string getHeaderForWrite()
+        {
+            string result = "";
+            string delimiter = "";
+            headers.ForEach(delegate(string cell)
+            {
+                result += delimiter + cell;
+                if (delimiter == "")
+                {
+                    delimiter = ",";
+                }
+            });
+            return result;
+        }
+    }
+}
---
+++ csvCompare.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csvDifferences
+{
+    class csvCompare
+    {
+        public csv a;
+        public csv b;
+        public List<int> colIndexToCompare;
+        public int colIndexToGroupBy;
+        public Dictionary<string, string> bufferFound;
+        public List<int> indexesNotMatched;
+        public List<int> indexesMatched;
+
+        public csvCompare(csv a, csv b, string[] headersToCompare, string headerToGroupBy, string pathToResult)
+        {
+            this.a = a;
+            this.b = b;
+            this.bufferFound = new Dictionary<string, string>();
+            this.indexesNotMatched = new List<int>();
+            this.indexesMatched = new List<int>();
+            this.colIndexToCompare = new List<int>();
+            string[] aHeaders = a.headers.ToArray<string>();
+            string[] bHeaders = b.headers.ToArray<string>();
+            if ((aHeaders.Length == bHeaders.Length) && (headersToCompare.Length < aHeaders.Length))
+            {
+                foreach (string header in headersToCompare)
+                {
+                    if (Array.IndexOf<string>(aHeaders, header) != -1)
+                    {
+                        this.colIndexToCompare.Add(Array.IndexOf<string>(aHeaders, header));
+                    }
+                }
+                if (Array.IndexOf<string>(aHeaders, headerToGroupBy) != -1)
+                {
+                    this.colIndexToGroupBy = Array.IndexOf<string>(aHeaders, headerToGroupBy);
+                }
+                for (int h = 0; h < a.rows.Count; h++)
+                {
+                    if (h % 100 == 0)
+                    {
+                        Console.WriteLine("Compared " + h + " / " + a.rows.Count);
+                    }
+                    List<string> row = a.rows.ElementAt(h);
+                    List<string> cellsStart = new List<string>();
+                    string grouping = row.ElementAt<string>(this.colIndexToGroupBy);
+                    this.colIndexToCompare.ForEach(delegate(int i)
+                    {
+                        cellsStart.Add(row.ElementAt<string>(i));
+                    });
+                    bool matched = false;
+                    for (int i = 0; i < b.rows.Count; i++)
+                    {
+                        List<string> cellsEnd = new List<string>();
+                        List<string> rowB = b.rows.ElementAt(i);
+                        for (int j = 0; j < this.colIndexToCompare.Count; j++)
+                        {
+                            cellsEnd.Add(rowB.ElementAt<string>(j));
+                        }
+                        List<string> afterCompareA = cellsStart.Except<string>(cellsEnd).ToList<string>();
+                        List<string> afterCompareB = cellsEnd.Except<string>(cellsStart).ToList<string>();
+                        if (afterCompareA.Count == 0 && afterCompareB.Count == 0)
+                        {
+                            matched = true;
+                            this.indexesMatched.Add(h);
+                            break;
+                        }
+                    }
+                    if (!matched)
+                    {
+                        this.indexesNotMatched.Add(h);
+                    }
+                }
+                if (this.indexesNotMatched.Count > 0)
+                {
+                    List<string> rowsToWrite = new List<string>();
+                    rowsToWrite.Add(a.getHeaderForWrite());
+                    for (int i = 0; i < this.indexesNotMatched.Count; i++)
+                    {
+                        rowsToWrite.Add(a.getRowForWrite(i));
+                    }
+                    string[] rowsArray = rowsToWrite.ToArray<string>();
+                    System.IO.File.WriteAllLines(pathToResult, rowsArray);
+                }
+            }
+        }
+    }
+}
